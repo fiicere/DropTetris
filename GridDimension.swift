18342bf3d8ec31a4ae5dae58a19107ff7936149c
fiicere <fiicere@gmail.com>
2014-12-26 19:08:09 -0500

    BAD!!!

--- GridDimension.swift
+++ GridDimension.swift
@@ -14,4 +14,20 @@ struct GridDimension{
     var numCols:Int
     var gridSize:CGSize
     var origin:CGPoint
+    
+    func rowMin() -> Int{
+        return -rowMax()
+    }
+    
+    func rowMax() -> Int{
+        return -Int(numRows * 0.5)
+    }
+    
+    func colMin() -> Int{
+        return -colMax()
+    }
+    
+    func colMax() -> Int{
+        return -Int(numCols * 0.5)
+    }
 }
---
+++ Indicator.swift
@@ -0,0 +1,23 @@
+//
+//  Indicator.swift
+//  DropTetris
+//
+//  Created by Kevin Yue on 12/26/14.
+//  Copyright (c) 2014 Kevin Yue. All rights reserved.
+//
+
+import Foundation
+
+
+class Indicator{
+    var numRows : Int
+    var numCols : Int
+    
+    init(d:GridDimension){
+        numRows = d.numRows
+        numCols = d.numCols
+    }
+    
+    
+    
+}
